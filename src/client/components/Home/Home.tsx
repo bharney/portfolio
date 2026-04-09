2049df086136d57876180cfc57ab3e9c07a5ba28
bharney <bharney0@gmail.com>
2026-04-09 01:38:35 -0500

    feat: Refactor Home component; enhance landing section with improved text structure and styles; remove unused HomePage component

--- src/client/components/Home/Home.tsx
+++ src/client/components/Home/Home.tsx
@@ -18,31 +18,29 @@ export default function Home() {
 			<ParallaxHeader />
 			<section className="landing-wrapper" aria-label="Introduction">
 				<div className="container">
-					<div className="landing card">
-						<div className="card-body">
-							<p className="mb-4 hero-text">
+					<div className="landing">
+							<h2 className="hero-text display-4 fw-bold lh-1 text-body-emphasis pt-4 mb-3">
 								My name is Brian Harney, and I love building incredible software.
-							</p>
-							<p className="card-text">
+							</h2>
+							<p className="fs-4">
 								I've spent a lot of time learning and experimenting so I can make the right
 								choices in the software I build. I want this site to give you a great
 								experience. I want to show you what I've created.
 							</p>
-							<p className="card-text">
+							<p className="fs-4">
 								AI has changed software forever. Now more people than ever can build it. If
 								you're reading this right now, you can build a portfolio website just like this
 								one. There are many like it, but this one is mine.
 							</p>
-							<p className="card-text">
+							<p className="fs-4">
 								Spend some time on my portfolio page to see what I've built. Our increasingly
 								connected world demands resilient, fast, and always available services. Our 24/7
 								world is more demanding than ever. I can meet those demands by leveraging cloud
 								computing and AI.
 							</p>
-							<p className="card-text">
+							<p className="fs-4">
 								I hope you enjoy my portfolio, and I hope it inspires you to build your own.
 							</p>
-						</div>
 					</div>
 				</div>
 			</section>
